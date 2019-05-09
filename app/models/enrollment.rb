56ba7a953616d63f39895ed0e5ddeaa15a6f7631
Amber McGregor <amber@vqhomes.com>
2019-05-09 18:43:02 +0000

    Styling for Course page

---
+++ app/models/enrollment.rb
@@ -0,0 +1,5 @@
+class Enrollment < ApplicationRecord
+    belongs_to :course
+    has_many :users
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def enrolled_in?(course)
+    # enrolled_courses[]
+    # enrollments.each do |enrollment|
+    #   enrolled_courses << enrollment.course
+    # end
+    # OR
+    # enrolled_courses = enrollments.collect do |enrollment|
+    #     enrollment.course
+    # end
+    # OR
+    # enrolled_courses = enrollments.collect(&:course)
+    # OR
+    # jsut add this above:    has_many :enrolled_courses, through: :enrollments, source: :course
+    return enrolled_courses.include?(course)
+  end
+
 end
